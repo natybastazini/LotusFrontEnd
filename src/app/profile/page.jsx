541f7b7cdaf799d430da18c77a94b4065efe3c6c
ALICE ZEURGO CRUZ <23111712@CFP127ALUNO.LOCAL>
2024-10-03 17:02:47 -0300

    Perfil

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -21,7 +21,6 @@ const SettingsModal = ({ isOpen, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-6 w-11/12 md:w-1/3">
         <h2 className="text-lg font-bold mb-4">Configurações</h2>
-        {/* Adicione aqui os campos ou opções que você deseja */}
         <button onClick={onClose} className="mt-4 bg-pink-3 text-white p-2 rounded">
           Fechar
         </button>
@@ -33,6 +32,8 @@ const SettingsModal = ({ isOpen, onClose }) => {
 export default function Home() {
   const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
   const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState(''); // Novo estado para email
+  const [password, setPassword] = useState(''); // Novo estado para senha
   const [userId, setUserId] = useState("");
   const [userInfo, setUserInfo] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -81,11 +82,6 @@ export default function Home() {
       };
       reader.readAsDataURL(file);
     }
-  };
-
-  const handleSaveProfilePicture = () => {
-    setProfilePicture(selectedImage);
-    toggleModal();
   };
 
   const handleSpecializationChange = (option) => {
@@ -140,12 +136,9 @@ export default function Home() {
         </button>
       </div>
 
-      {/* DIV CINZA PRINCIPAL */}
       <div className="bg-gray-1 flex-grow lg:w-[600%] rounded-3xl lg:ml-[20%] p-4 lg:p-8 relative">
         <div className="absolute top-0 left-0 w-full">
           <Image src={laranja} alt="Nav Laranja" className="w-full h-32 lg:h-24" />
-
-          {/* IMAGEM BOTAO CONFIG */}
           <button onClick={() => setIsSettingsModalOpen(true)}>
             <Image
               src={config}
@@ -153,7 +146,6 @@ export default function Home() {
               className="absolute right-16 top-2 w-9 h-16"
             />
           </button>
-
           <button onClick={toggleModal}>
             <Image
               src={edit}
@@ -229,14 +221,19 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-8 lg:mt-20 text-gray-3 lg:flex lg:justify-between">
-          <div className="text-center lg:text-left">
-            <h2 className="font-bold text-lg">Descrição</h2>
-            <p>{description}</p>
+        {/* DESCRIÇÃO DO PERFIL */}
+        <div className="flex mt-28 gap-28 justify-between">
+          <div className="flex-1">
+            <h1 className="text-gray-3 text-[23px] font-ABeeZee">
+              Doula a {yearsAsDoula ? yearsAsDoula : "5"} anos
+            </h1>
+            <h1 className="text-xl text-gray-4 break-words max-w-xl mt-4">
+              {description}
+            </h1>
           </div>
 
           {/* Conteúdos Publicados */}
-          <div className="flex-1">
+          <div className="flex-1 ">
             <h1 className="text-gray-3 text-[23px] font-ABeeZee">Conteúdos publicados:</h1>
             <div className="mt-4 space-y-4">
               <div className="flex items-start cursor-pointer border-2 border-gray-300 rounded-lg hover:bg-gray-200 bg-white">
@@ -253,66 +250,106 @@ export default function Home() {
                   <Image src={arrow} alt="Seta rosa" className="h-10 w-10" />
                 </div>
               </div>
-              {/* Adicione mais conteúdos publicados aqui */}
             </div>
           </div>
         </div>
       </div>
 
-      {/* Chamada do SettingsModal */}
       <SettingsModal isOpen={isSettingsModalOpen} onClose={() => setIsSettingsModalOpen(false)} />
 
-      {/* Modal para editar perfil */}
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-          <div className="bg-white rounded-lg p-6 w-11/12 md:w-1/3">
-            <h2 className="text-lg font-bold mb-4">Editar Perfil</h2>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="mb-4"
-            />
-            <input
-              type="text"
-              placeholder="CPF"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-              className="border rounded p-2 mb-4 w-full"
-            />
-            <input
-              type="text"
-              placeholder="Anos como doula"
-              value={yearsAsDoula}
-              onChange={(e) => setYearsAsDoula(e.target.value)}
-              className="border rounded p-2 mb-4 w-full"
-            />
-            <textarea
-              placeholder="Descrição"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="border rounded p-2 mb-4 w-full"
-            />
-            <div>
-              <h2 className="font-bold mb-2">Especializações</h2>
-              {specializationOptions.map((option) => (
-                <label key={option} className="flex items-center mb-2">
-                  <input
-                    type="checkbox"
-                    checked={selectedSpecializations.includes(option)}
-                    onChange={() => handleSpecializationChange(option)}
-                    className="mr-2"
-                  />
-                  {option}
-                </label>
-              ))}
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-25">
+          <div className="bg-gray-1  rounded-lg p-6 w-11/12 md:w-1/2 max-h-[80vh] overflow-y-auto">
+            <h2 className="text-lg font-ABeeZee text-gray-3 mb-4">Editar Perfil</h2>
+            <div className="flex items-start gap-4 mb-4">
+              <label className="cursor-pointer">  
+                <div className="h-48 w-48 bg-gray-200 border-2 border-dashed border-gray-400 rounded flex items-center justify-center">
+                  {selectedImage ? (
+                    <img
+                      src={selectedImage}
+                      alt="Imagem Selecionada"
+                      className="h-full w-full object-cover rounded"
+                    />
+                  ) : (
+                    <span className="text-gray-400">Adicionar imagem</span>
+                  )}
+                </div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+              </label>
+              <div className="flex flex-col gap-4 flex-grow">
+                <input
+                  type="text"
+                  placeholder="CPF"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                  className="border rounded p-2 mb-4 w-full"
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border rounded p-2 mb-4 w-full"
+                />
+                <input
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border rounded p-2 mb-4 w-full"
+                />
+                <input
+                  type="text"
+                  placeholder="Anos como doula"
+                  value={yearsAsDoula}
+                  onChange={(e) => setYearsAsDoula(e.target.value)}
+                  className="border rounded p-2 mb-4 w-full"
+                />
+                <textarea
+                  placeholder="Descrição"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="border rounded p-2 mb-4 w-full"
+                />
+                <div>
+                  <h2 className="font-ABeeZee text-gray-3 mb-2">Especializações</h2>
+                  {specializationOptions.map((option) => (
+                    <label key={option} className="flex items-center mb-2">
+                      <input
+                        type="checkbox"
+                        checked={selectedSpecializations.includes(option)}
+                        onChange={() => handleSpecializationChange(option)}
+                        className="mr-2"
+                      />
+                      {option}
+                    </label>
+                  ))}
+                </div>
+                {selectedSpecializations.length > 0 && (
+                  <div className="mt-4">
+                    <h3 className="font-ABeeZee text-gray-3">Especializações Selecionadas:</h3>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                      {selectedSpecializations.map((spec, index) => (
+                        <span key={index} className="bg-gray-3 text-white rounded-[100px] px-2 py-1 text-sm">
+                          {spec}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                )}
+                <button onClick={handleSave} className="mt-4 bg-pink-3 text-white p-2 w-52 rounded">
+                  Salvar
+                </button>
+                <button onClick={toggleModal} className="mt-4 bg-gray-300 text-gray-700 w-52 p-2 rounded">
+                  Cancelar
+                </button>
+              </div>
             </div>
-            <button onClick={handleSaveProfilePicture} className="mt-4 bg-pink-3 text-white p-2 rounded">
-              Salvar
-            </button>
-            <button onClick={toggleModal} className="mt-4 bg-gray-300 text-gray-700 p-2 rounded">
-              Cancelar
-            </button>
           </div>
         </div>
       )}
