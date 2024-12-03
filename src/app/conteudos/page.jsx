521a8f21a3642d01676482f252f80b334876a303
NATÁLIA BASTAZINI DURÃES DOS SANTOS <23111756@dsn127m1242269.cfp127aluno.local>
2024-12-03 11:31:57 -0300

    PRECISA ARRUMAR O BANCOOgit add .git add .

--- src/app/conteudos/page.jsx
+++ src/app/conteudos/page.jsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Logo from "../../../public/icons/utilities/lotus-icon.svg"
-import LogoVerMais from "../../../public/icons/utilities/add-circle.svg"
 import LogoLogout from "../../../public/icons/nav/logout.svg"
 
 // Navegção
